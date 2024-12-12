87ac699a5dbbe8a4cd26cd739a335407cf36954b
Lemesle Cyriaque <100539151+Cyriiaque@users.noreply.github.com>
2024-12-12 16:39:47 +0100

    Add files via upload

---
+++ api/Controller/MoviesController.php
@@ -0,0 +1,53 @@
+<?php
+require_once "Controller.php";
+require_once "Repository/MoviesRepository.php" ;
+
+class MoviesController extends Controller {
+
+    private MoviesRepository $movies;
+
+    public function __construct(){
+        $this->movies = new MoviesRepository();
+    }
+
+   
+    protected function processGetRequest(HttpRequest $request) {
+        $stat=$request->getParam("stat");
+
+        if ($stat=='currentmonth'){
+            $p= $this->movies->getStatCurrentMonth();
+            return $p==null ? false :  $p;
+        }
+
+        else if ($stat=='ranking'){
+            $p = $this->movies->getStatRanking();
+            return $p==null ? false :  $p;
+        }
+        else if ($stat=='history'){
+            $p = $this->movies->getStatHistory();
+            return $p==null ? false :  $p;
+        }
+        else if ($stat=='historybytypesales'){
+            $p = $this->movies->getStatHistoryByTypeSales();
+            return $p==null ? false :  $p;
+        }
+        else if ($stat=='historybytyperentals'){
+            $p = $this->movies->getStatHistoryByTypeRentals();
+            return $p==null ? false :  $p;
+        }
+        else if ($stat=='historybyid'){
+            $movieId = $request->getParam("id");
+            if ($movieId == null) {
+                return false;
+            }
+            $p = $this->movies->getStatHistoryById($movieId);
+            return $p == null ? false : $p;
+        }
+    }
+
+    protected function processPostRequest(HttpRequest $request) {
+        return false;
+    }
+   
+}
+?>
